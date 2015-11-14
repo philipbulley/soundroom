aae5ac99c315d5d574a6c69d0050ad985660f138
Ian McGregor <ian@mcgregor.codes>
2015-11-14 21:04:58 +0000

    Handles redirect after auth

--- server/controller/route/auth.js
+++ server/controller/route/auth.js
@@ -3,66 +3,82 @@ const express = require('express'),
 
 const router = express.Router();
 
+// redirect middleware saves Url to redirect to after auth
+
+function setRedirect(req, res, next) {
+  req.session.redirect = req.query.redirect;
+  next();
+}
+
+// http://passportjs.org/docs/authenticate#custom-callback
+
+function customCallback(provider) {
+  return (req, res, next) => {
+    // passport.authenticate(provider, (err, user, info) => {
+    passport.authenticate(provider, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        const {redirect} = req.session;
+        if (redirect) {
+          const url = user ? redirect : `${redirect}?error=denied`;
+          delete req.session.redirect;
+          return res.redirect(url);
+        } else {
+          return res.json(req.user || {message: 'Access denied'});
+        }
+      });
+    })(req, res, next);
+  };
+}
+
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
 router.route('/google')
-  .get(passport.authenticate('google', {
+  .get(setRedirect, passport.authenticate('google', {
     scope: ['https://www.googleapis.com/auth/plus.login']
-  }), (req, res) => {
-    // The request will be redirected to Google for authentication, so this
-    // function will not be called.
-  });
+  }));
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
+//   login page.  Otherwise, the primary route function function will be called.
+// router.route('/google/callback')
+//   .get(passport.authenticate('google'), authCallback);
+
 router.route('/google/callback')
-  .get(passport.authenticate('google'), (req, res) => {
-    // res.json({message: 'Log in successful'});
-    res.json(req.user);
-  });
+  .get(customCallback('google'));
+
 
 // GET /auth/spotify
 router.route('/spotify')
-  .get(passport.authenticate('spotify'));
+  .get(setRedirect, passport.authenticate('spotify'));
 
 router.route('/spotify/callback')
-  .get(passport.authenticate('spotify', {
-    failureRedirect: '/login'
-  }),
-  (req, res) => {
-    res.json(req.user);
-  });
+  .get(customCallback('spotify'));
+
 
 // GET /auth/twitter
 router.route('/twitter')
-  .get(passport.authenticate('twitter'));
+  .get(setRedirect, passport.authenticate('twitter'));
 
 router.route('/twitter/callback')
-  .get(passport.authenticate('twitter', {
-    failureRedirect: '/login'
-  }),
-  (req, res) => {
-    res.json(req.user);
-  });
+  .get(customCallback('twitter'));
 
 
 // GET /auth/facebook
 router.route('/facebook')
-  .get(passport.authenticate('facebook'));
+  .get(setRedirect, passport.authenticate('facebook'));
 
 router.route('/facebook/callback')
-  .get(passport.authenticate('facebook', {
-    failureRedirect: '/login'
-  }),
-  (req, res) => {
-    res.json(req.user);
-  });
+  .get(customCallback('facebook'));
 
 // GET /auth/logout
 router.route('/logout')
