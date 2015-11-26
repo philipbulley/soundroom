ed074ce0948a32234dd0f638dd9dc583adc3dfb0
Ian McGregor <ian@mcgregor.codes>
2015-11-26 23:16:30 +0000

    Abstracts track search into new controller

--- server/controller/route/search.js
+++ server/controller/route/search.js
@@ -1,7 +1,7 @@
-import _ from 'lodash';
 import express from 'express';
-import SpotifyService from '../../service/SpotifyService';
+import SearchController from '../SearchController';
 import { verify } from '../AuthController';
+
 
 const router = express.Router();
 
@@ -15,38 +15,9 @@ router.route('/:terms')
 
     console.log('GET /search/:terms', req.params.terms);
 
-    SpotifyService.getInstance().search(req.params.terms)
-      .then((result) => {
-        if (!result.numTracks) {
-          return res.json([]);
-        }
-        const tracks = [];
-        for (let i = 0; i < result.numTracks; i++) {
-          const track = result.getTrack(i);
-
-          const link = track.link;
-          const title = track.name;
-          const album = track.album.link;
-          // const image = SpotifyService.getInstance().getImage(album)
-          //   .then((img) => {
-          //     console.log('image:', img.length);
-          //   });
-          const artist = track.artists.reduce((val, nth) => {
-            return val ? `${val}, ${nth.name}` : nth.name;
-          }, '');
-
-          tracks.push({
-            artist,
-            title,
-            link,
-            album
-          });
-        }
-        res.json(_.uniq(tracks, (n) => n.artist + n.title));
-      })
-      .catch((err) => {
-        res.send(err);
-      });
+    new SearchController().search(req.params.terms)
+      .then((results) => res.json(results))
+      .catch((err) => res.send(err));
   });
 
-module.exports = router;
+export default router;
