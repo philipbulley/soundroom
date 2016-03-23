a437978ec2e9f40b9faa6c081f5d89b23aa068ea
Philip Bulley <philip@milkisevil.com>
2016-03-23 18:36:18 +0000

    FIxed route changing bug weirdly due to use of AsyncPipe in playlist-menu-item.html.

--- client/src/soundroom/component/playlist-menu-item/playlist-menu-item.component.ts
+++ client/src/soundroom/component/playlist-menu-item/playlist-menu-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, ChangeDetectionStrategy} from 'angular2/core';
+import {Component, Input, ChangeDetectionStrategy, ChangeDetectorRef} from 'angular2/core';
 import {RouterLink} from 'angular2/router';
 
 var alertify = require('alertify.js');
@@ -19,42 +19,77 @@ export class PlaylistMenuItemComponent {
   @Input()
   private playlist:Playlist;
 
-  private DELETE_CONFIRM_WAIT_SECS:number = 3;
-  private DELETE_CONFIRM_WAIT_EXIT_SECS:number = 5;
-  private errorMessage:string;
-  private isDeleting:boolean = false;
-  private isDeleteConfirm:boolean = false;
-  private deleteWait:Observable<number>;
-  private deleteWaitDisplay:Observable<number>;
+  /** To prevent trigger happy clicking, button will be disabled for this length of time before allowing input */
+  private DELETE_CONFIRM_DISABLED_SECS:number = 3;
 
-  constructor( private playlistService:PlaylistService ) {
+  /** Button will allow a click to execute delete for this length of time following the disabled period */
+  private DELETE_CONFIRM_ENABLED_SECS:number = 5;
+
+  /** A delete network request is in progress */
+  private DELETE_STATE_NETWORK = 'DELETE_STATE_NETWORK';
+
+  /** The full delete button is visible, but is not yet clickable */
+  private DELETE_STATE_CONFIRM_DISABLED = 'DELETE_STATE_CONFIRM_DISABLED';
+
+  /** The delete button can be clicked, doing so will execute a delete */
+  private DELETE_STATE_CONFIRM_ENABLED = 'DELETE_STATE_CONFIRM_ENABLED';
+
+  /** Holds the value of a `DELETE_STATE_*` constant */
+  private deleteState = null;
+
+  /** Observable that produces a counter based on the DELETE_CONFIRM_* constants */
+  private deleteConfirm:Observable<number>;
+
+  /** Number of seconds remaining until delete button becomes enabled */
+  private deleteDisabledSecs:number;
+
+
+  constructor( private playlistService:PlaylistService, private cdr:ChangeDetectorRef ) {
 
   }
 
   deleteMe() {
-    console.log('PlaylistMenuComponent.deleteMe()', this.playlist);
+    console.log('PlaylistMenuItemComponent.deleteMe()', this.playlist);
 
-    if (!this.isDeleteConfirm) {
+    if (!this.deleteState) {
 
-      this.isDeleteConfirm = true;
+      this.deleteState = this.DELETE_STATE_CONFIRM_DISABLED;
 
-      this.deleteWait = Observable
+      // The `deleteConfirm` Observable is no longer used with AsyncPipe in the template, as it was causing an error to be thrown when changing route in angular 2.0.0-beta.11
+      this.deleteConfirm = Observable
       // Fire initial timer value at 1ms, not 0ms. 0ms will dispatch first value synchronously, which for some reason
       // prevents AsyncPipe from updating. Not figured out why yet. Could this be a clue: http://www.bennadel.com/blog/3029-rxjs-streams-are-inconsistently-asynchronous-in-angular-2-beta-6.htm?source=epicenter
         .timer(1, 1000)
-        .take(this.DELETE_CONFIRM_WAIT_SECS + this.DELETE_CONFIRM_WAIT_EXIT_SECS + 1)
+        .take(this.DELETE_CONFIRM_DISABLED_SECS + this.DELETE_CONFIRM_ENABLED_SECS + 1)
         .map(secs => secs);
 
       // Observer for seconds before delete can be clicked
-      this.deleteWaitDisplay = this.deleteWait.map(( secs:number ) => Math.max(0, this.DELETE_CONFIRM_WAIT_SECS - secs));
+      this.deleteConfirm
+        .map(( secs:number ) => Math.max(0, this.DELETE_CONFIRM_DISABLED_SECS - secs))
+        .subscribe(secs => {
+          this.deleteDisabledSecs = secs;
+
+          console.log('this.deleteDisabledSecs', this.deleteDisabledSecs);
+
+          if (secs === 0) {
+            this.deleteState = this.DELETE_STATE_CONFIRM_ENABLED;
+          }
+
+          this.cdr.markForCheck();
+        });
 
       // Observer which will exit the delete confirmation, assuming user is not wanting to delete anymore
-      this.deleteWait.filter(( secs:number ) => secs === this.DELETE_CONFIRM_WAIT_SECS + this.DELETE_CONFIRM_WAIT_EXIT_SECS)
-        .subscribe(( secs:number ) => this.isDeleteConfirm = false);
+      this.deleteConfirm
+        .subscribe(( secs:number ) => {
+          if (secs === this.DELETE_CONFIRM_DISABLED_SECS + this.DELETE_CONFIRM_ENABLED_SECS) {
+            this.deleteState = null;
+          }
+          this.cdr.markForCheck();
+        });
 
     } else {
 
-      this.isDeleting = true;
+      this.deleteState = this.DELETE_STATE_NETWORK;
 
       return this.playlistService.deletePlaylist(this.playlist)
         .subscribe(( success ) => {
@@ -66,10 +101,10 @@ export class PlaylistMenuItemComponent {
           },
           error => {
             alertify.error("Can't delete \"" + this.playlist.name + "\". Try again later.");
-            this.isDeleting = false;
-            this.errorMessage = <any>error;
+            this.deleteState = null;
           });
-
     }
+
+
   }
 }
