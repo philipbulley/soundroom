e26f762c37165bdee85c379eaca5c9e49d16e005
Philip Bulley <philip@milkisevil.com>
2016-04-09 14:09:40 +0100

    Added other auth ID props to User

--- client/src/soundroom/model/factory/user.factory.ts
+++ client/src/soundroom/model/factory/user.factory.ts
@@ -7,13 +7,33 @@ export class UserFactory {
     var user = new User();
 
     user._id = apiData._id;
+
     user.name = apiData.name;
+
     user.avatar = apiData.avatar;
+
     user.created = new Date(apiData.created);
+
     user.modified = new Date(apiData.modified);
 
     if (apiData.hasOwnProperty('googleId')) {
       user.googleId = apiData.googleId;
+    }
+
+    if (apiData.hasOwnProperty('spotifyId')) {
+      user.spotifyId = apiData.spotifyId;
+    }
+
+    if (apiData.hasOwnProperty('facebookId')) {
+      user.facebookId = apiData.facebookId;
+    }
+
+    if (apiData.hasOwnProperty('twitterId')) {
+      user.twitterId = apiData.twitterId;
+    }
+
+    if (apiData.hasOwnProperty('userId')) {
+      user.userId = apiData.userId;
     }
 
     return user;
--- client/src/soundroom/model/user.ts
+++ client/src/soundroom/model/user.ts
@@ -1,10 +1,23 @@
 export class User {
-  
+
   _id:string;
+
   name:string;
+
   avatar:string;
+
   created:Date;
+
   modified:Date;
+
   googleId:string;
-  
+
+  spotifyId:string;
+
+  facebookId:string;
+
+  twitterId:string;
+
+  userId:string;
+
 }
