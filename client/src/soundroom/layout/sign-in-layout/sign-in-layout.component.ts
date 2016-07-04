16bbb26e8fc7b617efa7b3d576ddddba0448b22e
Philip Bulley <philip@milkisevil.com>
2016-07-04 12:32:55 +0100

    Fixes obtaining JWT via new router 3.0.0-beta.2

--- client/src/soundroom/layout/sign-in-layout/sign-in-layout.component.ts
+++ client/src/soundroom/layout/sign-in-layout/sign-in-layout.component.ts
@@ -1,5 +1,5 @@
 import {Component, ChangeDetectionStrategy, OnInit} from '@angular/core';
-import {ActivatedRoute, ROUTER_DIRECTIVES} from '@angular/router';
+import {Router, ActivatedRoute, ROUTER_DIRECTIVES} from '@angular/router';
 
 import {Observable} from 'rxjs/Observable';
 import {Store} from '@ngrx/store';
@@ -23,25 +23,34 @@ export class SignInLayout implements OnInit {
 
   private auth:Observable<Auth>;
 
-  constructor( private store:Store<AppState>, private route:ActivatedRoute, private authService:AuthService ) {
+  constructor(private store:Store<AppState>, private route:ActivatedRoute, private router:Router, private authService:AuthService) {
 
     this.auth = <Observable<Auth>>store.select('auth');
-    this.auth.subscribe(( auth:Auth ) => console.log('SignInLayout.auth: Auth:', auth));
+    this.auth.subscribe((auth:Auth) => console.log('SignInLayout.auth: Auth:', auth));
 
   }
 
   ngOnInit():any {
 
     // Check whether we've been passed a new JWT via the query string
-    let jwt = this.route.snapshot.params['jwt'];
 
-    if (jwt) {
-      this.authService.jwt = jwt;
-    }
+    // TODO: It should be possible to get jwt from ActivatedRoute.snapshot, but doesn't seem to work with @angular/router 3.0.0-beta.2 — for now we can subscribe to router Observable
+    // let jwt = this.route.snapshot.params['jwt'];
+    // console.log('SignInLayout.ngOnInit: jwt:', this.route.snapshot);
 
-    // TODO: Handle `error=denied` query string keypair?
+    this.router
+      .routerState
+      .queryParams
+      .subscribe((params:any) => {
 
-    this.authService.load();
+        if (params.jwt) {
+          this.authService.jwt = params.jwt;
+        }
+
+        // TODO: Handle `error=denied` query string keypair?
+
+        this.authService.load();
+      });
 
   }
 
