eccaf3b7e2c569d96796efbf0df48a6098615351
Philip Bulley <philip@milkisevil.com>
2017-04-10 18:32:01 +0100

    Removes SLOW_CONNECTION_RETRIES

--- client/src/soundroom/shared/service/network.service.ts
+++ client/src/soundroom/shared/service/network.service.ts
@@ -15,13 +15,6 @@ export class NetworkService {
    */
   private MAX_RETRY_INTERVAL: number = 30;
 
-  /**
-   * On this number of retries (within the exponential backoff strategy), we will emit our slow connection. This may be
-   * communicated to the user via UI.
-   * @type {number}
-   */
-  private SLOW_CONNECTION_RETRIES: number = 2;
-
   constructor(private store$: Store<AppState>) {
 
   }
@@ -34,17 +27,10 @@ export class NetworkService {
    *     Observable.retryWhen(errors => this.retry(errors))
    *
    * @param errors
-   * @returns {Observable<R>}
    */
   retry(errors: Observable<any>): Observable<any> {
     return errors
       .mergeMap((err, count) => {
-        // Emit event if we've retried SLOW_CONNECTION_RETRIES times
-        if (count === this.SLOW_CONNECTION_RETRIES) {
-          console.warn('TODO: Slow Connection: Implement slow connection reducer logic');
-          // this.onSlowConnection.emit(true);
-        }
-
         // Calc number of seconds we'll retry in using incremental backoff
         const retrySecs = Math.min(Math.round(Math.pow(++count, 2)), this.MAX_RETRY_INTERVAL);
         console.warn(`NetworkService.retry: Retry ${count} in ${retrySecs} seconds`);
@@ -53,14 +39,6 @@ export class NetworkService {
         return Observable.of(err)
           .delay(retrySecs * 1000);
       });
-  }
-
-  /**
-   * Other network reliant services should call this when the network has just completed a task.
-   * If we're slowConnection state, it will be brought back to normal.
-   */
-  ok() {
-    // TODO: Implement
   }
 
   /**
