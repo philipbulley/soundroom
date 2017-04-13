17e60a312ed3f09a6df057575d0d8f6d8212fddb
Philip Bulley <philip@milkisevil.com>
2017-04-13 09:53:12 +0100

    Image is now an interface

--- client/src/soundroom/shared/model/factory/image.factory.ts
+++ client/src/soundroom/shared/model/factory/image.factory.ts
@@ -1,20 +1,14 @@
 import { Image } from "../image";
 
 export class ImageFactory {
-
   static createFromApiResponse(apiData: any): Image {
+    const {_id, url, width, height} = apiData;
 
-    const image = new Image();
-
-    image._id = apiData._id;
-
-    image.url = apiData.url;
-
-    image.width = apiData.width;
-
-    image.height = apiData.height;
-
-    return image;
+    return {
+      _id,
+      url,
+      width,
+      height,
+    };
   }
-
 }
