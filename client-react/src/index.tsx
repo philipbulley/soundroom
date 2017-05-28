0b6b46a18c3260d8d27b2ed4929d0e55e063cf2e
Philip Bulley <philip@milkisevil.com>
2017-05-28 22:47:59 +0100

    wip: case change

--- client-react/src/index.tsx
+++ client-react/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './app/App';
+import App from './app/app-case-change-wip';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
