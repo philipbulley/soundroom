b4348812be6114a81f6d111c342744ed6ef5b7e7
Philip Bulley <philip@milkisevil.com>
2017-06-02 19:14:24 +0100

    Adds tests for loadUserSuccessCommand

---
+++ client-react/src/app/shared/store/auth/load-user-success/load-user-success.command.spec.ts
@@ -0,0 +1,27 @@
+import { Auth } from '../auth';
+import { AuthStatus } from '../auth-state';
+import { loadUserSuccessCommand } from './load-user-success.command';
+import { MOCK_USER } from '../../../user/user.mock';
+const deepFreeze = require('deep-freeze');
+
+describe('loadUserSuccessCommand', () => {
+  let authState: Auth;
+
+  beforeEach(() => {
+    authState = deepFreeze({
+      status: AuthStatus.LOADING,
+      user: null,
+      jwt: null,
+    });
+  });
+
+  it('should set status to logged in', () => {
+    authState = loadUserSuccessCommand(authState, MOCK_USER);
+    expect(authState.status).toBe(AuthStatus.LOGGED_IN);
+  });
+
+  it('should not contain an error', () => {
+    authState = loadUserSuccessCommand(authState, MOCK_USER);
+    expect(authState.error).toBeUndefined();
+  });
+});
---
+++ client-react/src/app/shared/user/user.mock.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export const MOCK_USER: User = {
+  _id: 'mock-user-id',
+  name: 'michael jackson',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg',
+  created: new Date('Thu May 25 2017 14:57:31 GMT+0100 (BST)'),
+  modified: new Date('Fri Jun 02 2017 19:09:31 GMT+0100 (BST)'),
+  googleId: 'mock-google-id',
+  spotifyId: 'mock-spotify-id',
+  facebookId: 'mock-facebook-id',
+  twitterId: 'mock-twitter-id',
+  userId: 'mock-user-id',
+};
