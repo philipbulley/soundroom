ee7a61c367104aa39a392881410d6e44c4e9763f
Philip Bulley <philip@milkisevil.com>
2017-06-04 20:48:50 +0100

    Attempt to use @storybook/addon-knobs - not working, but commented out

--- client-react/src/app/shared/icon/icon.stories.tsx
+++ client-react/src/app/shared/icon/icon.stories.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import './../../../index.css';
 import { storiesOf } from '@storybook/react';
+// import { withKnobs, number } from '@storybook/addon-knobs';
 import Icon from './icon';
+
+// const largerSizesSizeKnob = number('Size', 5, {
+//   range: true,
+//   min: 2,
+//   max: 5,
+//   step: 1,
+// });
 
 storiesOf('Icon', module)
   .addWithInfo(
@@ -11,9 +19,9 @@ storiesOf('Icon', module)
   )
   .addWithInfo(
     'larger sizes',
-    `Icon allows you to use Font Awesome icons within the app. 
-    The id prop should be the usual font awesome icon name. The size prop can be between 2 and 5 as per 
+    `Icon allows you to use Font Awesome icons within the app.
+    The id prop should be the usual font awesome icon name. The size prop can be between 2 and 5 as per
     font awesome's larger icons (http://fontawesome.io/examples/#larger)`,
-    () => (<Icon id="spotify" size="5"/>),
+    () => (<Icon id="spotify" size={5}/>),
   );
 
