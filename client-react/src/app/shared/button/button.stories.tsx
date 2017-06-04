1d525a7c110ca81b727b66363605807e7e3986b3
Philip Bulley <philip@milkisevil.com>
2017-06-04 22:12:34 +0100

    Button accepts props for styling

--- client-react/src/app/shared/button/button.stories.tsx
+++ client-react/src/app/shared/button/button.stories.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import './../../../index.css';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import Button from './button';
+import colors from '../colors/colors';
 
 storiesOf('Button', module)
   .addWithInfo(
     'default',
     `A default button.`,
     () => (
-      <Button>Click me!</Button>
+      <Button onClick={action('clicked')}>Click me!</Button>
+    ),
+  )
+  .addWithInfo(
+    'green',
+    `A green button.\n\nNOTE: Placed on darker background for Storybook illustrative purposes only.`,
+    () => (
+      <div style={{backgroundColor: colors.greyDust, padding: '20px', margin: '-20px'}}>
+        <Button green onClick={action('clicked')}>Click me!</Button>
+      </div>
     ),
   );
--- client-react/src/app/shared/button/button.tsx
+++ client-react/src/app/shared/button/button.tsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 import colors from '../colors/colors';
 import { darken } from 'polished';
 import ButtonReset from './button-reset';
+import { SyntheticEvent } from 'react';
 
-const Button = ({children}: Props) => (
-  <ButtonStyled>
+const Button = ({children, onClick, ...rest}: Props) => (
+  <ButtonStyled {...rest} onClick={onClick}>
     {children}
   </ButtonStyled>
 );
 
 interface Props {
   children: any;
+  onClick?: (event: SyntheticEvent<HTMLButtonElement>) => void;
+  green?: boolean;
+}
+
+interface ButtonStyledProps {
+  green?: boolean;
 }
 
 // TODO(styled-components): Remove square brackets when issue fixed:
@@ -20,17 +27,31 @@ const ButtonStyled = ButtonReset['extend']`
   padding: 14px 30px;
 
   border-radius: 4px;
-  color: ${colors.greyGrime};
-  background: ${colors.greyDust};
+  color: ${(props: ButtonStyledProps) => getColor(props).color};
+  background: ${(props: ButtonStyledProps) => getColor(props).backgroundColor};
   font-weight: bold;
   transition: background-color .4s linear, color .4s linear;
   outline: 0;
 
   &:hover {
-    background-color: ${darken(0.2, colors.greyDust)};
-    color: ${colors.greyDust};
+    background-color: ${(props: ButtonStyledProps) => darken(0.2, getColor(props).color)};
+    color: ${(props: ButtonStyledProps) => getColor(props).backgroundColor};
     transition: background-color 0s, color 0s;
   }
 `;
+
+function getColor(props: ButtonStyledProps) {
+  if (props.green) {
+    return {
+      color: colors.green,
+      backgroundColor: colors.white,
+    };
+  }
+
+  return {
+    color: colors.greyGrime,
+    backgroundColor: colors.greyDust,
+  };
+}
 
 export default Button;
