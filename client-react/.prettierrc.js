f031ca92f719d6d031ccf43c5032d873de98da64
Philip Bulley <philip@milkisevil.com>
2018-08-22 19:00:48 +0100

    Adds .prettierrc.js for benefit of IDEs not aware of prettier config in tslint.json

---
+++ client-react/.prettierrc.js
@@ -0,0 +1,8 @@
+// This is for the benefit of any IDEs that have prettier integration
+// These settings should also be mirrored in tslint.json until
+// I find a way to get tslint to read from .prettierrc.js
+module.exports = {
+  singleQuote: true,
+  useTabs: true,
+  printWidth: 120
+};
