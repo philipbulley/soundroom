cab6e1972ea7726549936af25a4839393d4ed87f
Philip Bulley <philip@milkisevil.com>
2018-08-22 19:14:02 +0100

    Removes GSAP from Confirmation, general tidy

--- client-react/src/app/playlist-menu/playlist-create/confirmation/confirmation.tsx
+++ client-react/src/app/playlist-menu/playlist-create/confirmation/confirmation.tsx
@@ -2,14 +2,6 @@ import * as React from 'react';
 import Icon from '../../../shared/icon/icon';
 import styled from 'styled-components';
 import { PlaylistCreate } from '../../../shared/store/playlists/playlists';
-import {
-	// TweenMax,
-	TimelineLite,
-	TimelineMax
-	// Expo
-} from 'gsap';
-// import TweenConfig = gsap.TweenConfig;
-
 import Button from '../../../shared/button/button';
 import { ConfirmationViewState } from './confirmation-view-state';
 import Back from './back';
@@ -19,50 +11,12 @@ import Spinner from './spinner';
 import { Layer } from './confirmation.styled';
 
 export class Confirmation extends React.Component<Props, State> {
-	spinner: HTMLElement;
-	tick: HTMLElement;
-	cross: HTMLElement;
-	backButton: HTMLElement;
-
-	tl: TimelineLite = null;
-
 	state = {
 		viewState: null,
 		viewStatePrev: null
 	};
 
-	constructor(props: Props) {
-		super(props);
-
-		this.state = {
-			viewState: this.getViewState(props.playlistCreate),
-			viewStatePrev: null
-		};
-	}
-
-	componentDidMount() {
-		const viewState = this.getViewState(this.props.playlistCreate);
-
-		this.transition(viewState, null);
-	}
-
-	componentDidUpdate(prevProps: Props) {
-		const viewState = this.getViewState(this.props.playlistCreate);
-		const viewStatePrev = prevProps.playlistCreate && this.getViewState(prevProps.playlistCreate);
-
-		if (viewState !== viewStatePrev) {
-			this.transition(viewState, viewStatePrev);
-		}
-	}
-
-	componentWillReceiveProps(nextProps: Props) {
-		this.setState({
-			viewState: this.getViewState(nextProps.playlistCreate),
-			viewStatePrev: this.props.playlistCreate && this.getViewState(this.props.playlistCreate)
-		});
-	}
-
-	getViewState(playlistCreate: PlaylistCreate): ConfirmationViewState {
+	static deriveViewState(playlistCreate: PlaylistCreate): ConfirmationViewState {
 		/** awaitingCreation signifies we're loading, or just about to load */
 		const awaitingCreation = playlistCreate.loading || (!playlistCreate.successfullyCreatedId && !playlistCreate.error);
 
@@ -75,102 +29,29 @@ export class Confirmation extends React.Component<Props, State> {
 		return ConfirmationViewState.CREATING_ROOM;
 	}
 
-	transition(viewState: ConfirmationViewState, viewStatePrev: ConfirmationViewState) {
-		// const { onSuccessComplete } = this.props;
+	constructor(props: Props) {
+		super(props);
 
-		if (this.tl) {
-			this.tl.kill();
-		}
-
-		this.tl = new TimelineMax();
-
-		// switch (viewStatePrev) {
-		// 	// case ConfirmationViewState.CREATING_ROOM:
-		// 	// 	this.tl.add(
-		// 	// 		TweenMax.to(this.spinner, 0.4, {
-		// 	// 			autoAlpha: 0,
-		// 	// 			y: -20,
-		// 	// 			ease: Expo.easeIn
-		// 	// 		})
-		// 	// 	);
-		// 	// 	break;
-		// 	case ConfirmationViewState.SUCCESSFUL:
-		// 		this.tl.add(
-		// 			TweenMax.to(this.tick, 0.4, { autoAlpha: 0, ease: Expo.easeOut })
-		// 		);
-		// 		break;
-		// 	case ConfirmationViewState.ERROR:
-		// 		this.tl.add(
-		// 			TweenMax.to([this.cross, this.backButton], 0.4, {
-		// 				autoAlpha: 0,
-		// 				ease: Expo.easeOut
-		// 			})
-		// 		);
-		// 		break;
-		// }
-
-		// switch (viewState) {
-		// 	// case ConfirmationViewState.CREATING_ROOM:
-		// 	// 	this.tl.add(
-		// 	// 		TweenMax.fromTo(
-		// 	// 			this.spinner,
-		// 	// 			0.4,
-		// 	// 			{ y: 0, scale: 0.8 },
-		// 	// 			{ autoAlpha: 1, scale: 1, ease: Expo.easeOut }
-		// 	// 		)
-		// 	// 	);
-		// 	// 	break;
-		// 	case ConfirmationViewState.SUCCESSFUL:
-		// 		const config: TweenConfig = { autoAlpha: 1, y: 0, ease: Expo.easeOut };
-		// 		this.tl.add(TweenMax.fromTo(this.tick, 0.4, { y: 20 }, config));
-		// 		this.tl.add(
-		// 			TweenMax.to(this.tick, 0.6, {
-		// 				y: -20,
-		// 				autoAlpha: 0,
-		// 				ease: Expo.easeIn,
-		// 				onComplete: () => {
-		// 					if (onSuccessComplete) {
-		// 						onSuccessComplete();
-		// 					}
-		// 				}
-		// 			}),
-		// 			null,
-		// 			null,
-		// 			1.2
-		// 		);
-		// 		break;
-		// 	case ConfirmationViewState.ERROR:
-		// 		this.tl.add(
-		// 			TweenMax.fromTo(
-		// 				this.cross,
-		// 				0.4,
-		// 				{ scale: 0.8 },
-		// 				{ autoAlpha: 1, scale: 1, y: 0, ease: Expo.easeOut }
-		// 			)
-		// 		);
-		// 		this.tl.add(
-		// 			TweenMax.fromTo(
-		// 				this.backButton,
-		// 				0.4,
-		// 				{ scale: 0.8 },
-		// 				{ autoAlpha: 1, scale: 1, y: 0, ease: Expo.easeOut }
-		// 			)
-		// 		);
-		// 		break;
-		// }
+		this.state = {
+			viewState: Confirmation.deriveViewState(props.playlistCreate),
+			viewStatePrev: null
+		};
 	}
 
-	// TODO: TEst this works by creating a playlist!
+	componentWillReceiveProps(nextProps: Props) {
+		this.setState({
+			viewState: Confirmation.deriveViewState(nextProps.playlistCreate),
+			viewStatePrev: this.props.playlistCreate && Confirmation.deriveViewState(this.props.playlistCreate)
+		});
+	}
+
 	/**
 	 * To be called when the success ConfirmationViewState.SUCCESSFUL animation has completed. The parent of this
 	 * component can then proceed.
 	 */
 	onSuccessComplete = () => {
-		const { onSuccessComplete } = this.props;
-		const { viewState } = this.state;
-
-		if (ConfirmationViewState.SUCCESSFUL === viewState && onSuccessComplete) {
-			onSuccessComplete();
+		if (ConfirmationViewState.SUCCESSFUL === this.state.viewState && this.props.onSuccessComplete) {
+			this.props.onSuccessComplete();
 		}
 	};
 
